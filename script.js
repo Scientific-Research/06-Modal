ca85cb97278083f6d3f3ad8666f4e14d89690aa4
Maximilian <scientificresearch309@gmail.com>
2024-10-22 13:32:14 +0200

    writing some comments and doing the toggle state

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ const showWindow = document.querySelector('.show-modal');
 const modal = document.querySelector('.modal');
 const closeWindow = document.querySelector('.close-modal');
 
+/* As default, we don't want to see the Window, only after clicking on the button, we want to display it! */
+modal.style.display = 'none';
+
+/* here, we have a toggle state, we can click on the same button and it will close or show our window! */
 showWindow.addEventListener('click', () => {
-  modal.style.display = 'block';
+  modal.style.display === 'none'
+    ? (modal.style.display = 'block')
+    : (modal.style.display = 'none');
 });
 
+/* here, we click on the X symbol and the window will be closed! */
 closeWindow.addEventListener('click', () => {
   modal.style.display = 'none';
 });
